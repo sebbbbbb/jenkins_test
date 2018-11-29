2c2403641bf5530f48306f1d04019932da779230
Sébastien Pécoul <specoul@steamulo.com>
2018-11-29 16:07:57 +0100

    [ADD] .

--- lib/jenkins_test.rb
+++ lib/jenkins_test.rb
@@ -18,6 +18,26 @@ module JenkinsTest
       `rm -rf ./Build/*`
       `rm -rf ~/Library/Developer/Xcode/DerivedData/zouzous-tvos*`
     end
+
+    def ut
+      `set -o pipefail && env NSUnbufferedIO=YES`
+      `xcodebuild -workspace zouzous-tvos.xcworkspace \
+        -scheme zouzous-tvos \
+        -configuration Debug \
+        -destination 'platform=tvOS Simulator,name=Apple TV 4K' \
+        -derivedDataPath './Build/Test_output/' \
+        -enableCodeCoverage YES clean build test`
+      
+      `xsltproc -o ./Build/Test_output/report.junit ./scripts/plist_to_junit.xsl ./Build/Test_output/Logs/Test/*.xcresult/TestSummaries.plist`
+      
+    end
+
+    def cocoapods
+      `rvm 2.3.3 do bundle exec pod update`
+      `rvm 2.3.3 do bundle exec pod install`
+    end
+
+
   end
 
 end
